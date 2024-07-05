8e31477cda3248ac84ede9d63a884f034a7ba54e
Izzamuzzic <zwq651406441@gmail.com>
2024-07-05 15:30:13 +0800

    Add unit tests

--- mongo.js
+++ mongo.js
@@ -20,11 +20,6 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model("Note", noteSchema);
 
-const note = new Note({
-  content: "HTML is easy",
-  important: true,
-});
-
 Note.find({}).then((result) => {
   result.forEach((note) => {
     console.log(note);
