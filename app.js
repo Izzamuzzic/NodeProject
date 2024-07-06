afc977c2738c028ea697528f3fe6bda62f805e49
Izzamuzzic <zwq651406441@gmail.com>
2024-07-06 18:38:24 +0800

    Add Token-based authentication

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const app = express();
 const cors = require("cors");
 const notesRouter = require("./controllers/notes");
 const userRouter = require("./controllers/users");
-const middleware = require("./utils/middleware");
+const loginRouter = require("./controllers/login");
+const middleware = require("./utils/middleware.js");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
 
@@ -29,6 +30,7 @@ app.use(middleware.requestLogger);
 
 app.use("/api/notes", notesRouter);
 app.use("/api/users", userRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- utils/middleware.js
+++ utils/middleware.js
@@ -26,8 +26,9 @@ const errorHandler = (error, request, response, next) => {
     return response
       .status(400)
       .json({ error: "expected `username` to be unique" });
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(400).json({ error: "token missing or invalid" });
   }
-
   next(error);
 };
 
